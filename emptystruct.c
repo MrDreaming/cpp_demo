249d39e19f784381f06ce9ffd48e8a0c12b2e2ca
MrJasonCompany <zhiying_zheng@sina.com>
2017-11-28 21:56:44 +0800

    定义一个空的结构体

--- emptystruct.c
+++ emptystruct.c
@@ -4,7 +4,7 @@ struct osip_thread;
 
 int main(){
 	struct osip_thread* pdemoThread; // no error , no warming
-	struct osip_thread demoThread; // error: storage size of ¡®demoThread¡¯ isn¡¯t known 
+	struct osip_thread demoThread; // error storage size of 'demoThread' isn't known
 	printf("Test empty struct ! \n");
 	return 0;
 }
